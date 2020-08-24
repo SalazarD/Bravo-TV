8bf1a5f7ee1b80184527cf55c2a61bbe9ba092fa
Rohan Desai <rrdesai93@gmail.com>
2020-08-24 15:40:11 -0400

    Added DbCon.java

---
+++ src/com/utilities/DbCon.java
@@ -0,0 +1,63 @@
+package com.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DbCon {
+
+	private static Connection con = null;
+
+	private static final String userName = "root";
+
+	private static final String password = "";
+
+	private static final String serverName = "localhost";
+
+	private static final int portNumber = 3306;
+
+	private static final String dbName = "";
+
+	private static final String tableName = "";
+
+	public static Connection getConnection()
+	{
+		if(con == null)
+		{
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+
+				Properties connectionProps = new Properties();
+				connectionProps.put("user", userName);
+				connectionProps.put("password", password);
+
+				con = DriverManager.getConnection("jdbc:mysql://"
+						+ serverName + ":" + portNumber + "/" + dbName,
+						connectionProps);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return con;
+	}
+
+	public static void closeConnection()
+	{
+		try
+		{
+			if(con != null && !con.isClosed())
+			{
+				con.close();
+			}
+			con = null;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
