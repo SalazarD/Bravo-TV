1abc631969a8707c466226442a17bda38542adb3
Dan <12995058+danielhac@users.noreply.github.com>
2020-08-26 17:14:12 -0700

    added missing file

---
+++ src/com/dao/RetailerDao.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import com.bean.Retailer;
+
+public class RetailerDao extends AbstractDao<Retailer> {
+
+	@Override
+	protected String getTableName() {
+		return "CASE_Retailer";
+	}
+
+	@Override
+	protected Retailer getNewBean() {
+		return new Retailer();
+  }
+
+}
